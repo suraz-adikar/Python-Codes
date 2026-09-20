e58f25204e10bd26032b416525aaa2ff0be43fdd
Suraj <surajadhikari1009@gmail.com>
2026-09-20 19:44:08 +0530

    Using Different logics for fun

---
+++ Day 1 Arithematic.py
@@ -0,0 +1,50 @@
+a=1
+b=2
+c=a-b
+print(c)
+
+d="Bool"
+e="Nice"
+f=d+e
+print(f)
+
+
+g="Bol "
+h=5
+i=g*h
+print(i)
+
+
+z=10
+j=2
+print(z/j)
+print(z//j)
+
+m=22
+n=7
+print(m/n)
+print(m//n)
+
+
+print(m>n)
+print(m==n)
+
+
+age = 25
+
+print(age == 25)
+print(age == 30)
+print(age != 30)
+print(age > 18)
+print(age < 18)
+
+
+#using and , or and not
+
+age=23
+print(age>18 and age<24)
+
+print(age>23 or age<2)
+
+
+print(not age)
