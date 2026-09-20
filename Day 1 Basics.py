266893890491de91b8c8d6d9b54abbd8cdbea5b6
Suraj <surajadhikari1009@gmail.com>
2026-09-20 19:44:08 +0530

    Basics for Everything

---
+++ Day 1 Basics.py
@@ -0,0 +1,34 @@
+'''Keywords-> Pythons Build in syntax'''
+'''Fuctions-> Reuseable Piece of Code'''
+'''Operators-> Use for Compairing two or more values'''
+'''variable -> A name that refers to the value we wrote'''
+'''Parameter -> It is a variable which is defined by the fuctions that receive information'''
+'''Arguments -> the real value that we give to parameter when calling a fuction'''
+'''Objects -> An object is a value/data that Python can work with.'''
+'''Method ->  A method is a function that belongs to an object/type.'''
+'''Expression -> An expression is something Python evaluates to produce a value.'''
+'''Attribute -> An attribute is data/information associated with an object.'''
+'''Statement ->A statement is an instruction Python executes.'''
+
+
+
+
+
+'''Keywords-> loops-in,for,while,else,not,class,from,lambda,continue,break'''
+'''Fuction-> def,print'''
+'''Operators ->+,-,/,//,%'''
+'''Variables -> A=10 here A is variables'''
+'''Parameters -> print(*Groups,sep="") and def school (class,roll no) inside ()is parameters'''
+'''Arguments -> def school(class):
+                    print("Hello,class)
+                school (10)     here 10 is argument'''
+'''objects -> A=10 here 10 is object'''
+'''Methods ->a="suraj" 
+             a.upper() here upper is Methods'''
+'''Expression-> A+B this whole is Expression'''
+'''Attribute -> df.shape here df is attribute looks like object.attribute arr.shape
+arr.ndim
+arr.size
+arr.dtype
+'''
+'''Statement ->A=10 Store/refer to 10 using the name x.'''
