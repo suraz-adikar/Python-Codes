790112ff9ee3b15f46dbc9a13dfde58ddc933789
Suraj <surajadhikari1009@gmail.com>
2026-09-24 20:01:54 +0530

    Knowing Set

--- Day 2 Loops Understanding.py
+++ Day 2 Loops Understanding.py
@@ -2,3 +2,7 @@
 '''For-> Used when we know the amount of time we have to use'''
 '''While->When we dont know how many times loop will work'''
 '''if->Conditions in loops'''
+'''Basic Structure
+for variable in iterable:
+    code
+    '''
